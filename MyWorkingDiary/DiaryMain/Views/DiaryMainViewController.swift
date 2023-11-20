79f0887385877cd7d8bc374ff6cb5f0608c7256c
Bucky <bucky5683@gmail.com>
2023-11-20 12:22:52 +0900

    Add package

---
+++ MyWorkingDiary/DiaryMain/Views/DiaryMainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiaryMainViewController.swift
+//  MyWorkingDiary
+//
+//  Created by 김서연 on 2023/11/20.
+//
+
+import UIKit
+
+class DiaryMainViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
