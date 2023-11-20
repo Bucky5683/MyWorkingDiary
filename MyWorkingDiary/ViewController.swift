a80b26a8a423c843b01b73afe66f44a7141eb727
Bucky <bucky5683@gmail.com>
2023-11-20 11:54:00 +0900

    Initial Commit

---
+++ MyWorkingDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWorkingDiary
+//
+//  Created by 김서연 on 2023/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
